10598f72424c45b2dd3b86a3fa6466e9f07f4fb7
Blaine Richardson <blainerrichardson@gmail.com>
2021-05-24 16:01:31 -0400

    feat: section-2: finishing section-2

--- lib/infrastructure-as-code-master-aws-cloud-development-kit-cdk-stack.ts
+++ lib/infrastructure-as-code-master-aws-cloud-development-kit-cdk-stack.ts
@@ -1,8 +1,13 @@
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 
 export class InfrastructureAsCodeMasterAwsCloudDevelopmentKitCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const bucket = new Bucket(this, 'MySimpleAppBucket', {
+      encryption: BucketEncryption.S3_MANAGED
+    });
 
     // The code that defines your stack goes here
   }
