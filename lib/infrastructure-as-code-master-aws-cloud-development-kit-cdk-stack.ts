8732cdc08a94d9307bff0785b2168f701bac36b3
Blaine Richardson <blainerrichardson@gmail.com>
2021-05-25 18:01:52 -0400

    feat:section-6: finishing

--- lib/infrastructure-as-code-master-aws-cloud-development-kit-cdk-stack.ts
+++ lib/infrastructure-as-code-master-aws-cloud-development-kit-cdk-stack.ts
@@ -3,6 +3,7 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda-nodejs';
 import { Runtime } from '@aws-cdk/aws-lambda';
 import * as path from 'path';
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
 
 export class InfrastructureAsCodeMasterAwsCloudDevelopmentKitCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -12,10 +13,20 @@ export class InfrastructureAsCodeMasterAwsCloudDevelopmentKitCdkStack extends cd
       encryption: BucketEncryption.S3_MANAGED
     });
 
+    const bucketDeployment = new BucketDeployment(this, 'MySimplePhotos', {
+      sources: [
+        Source.asset(path.join(__dirname, '..', 'photos'))
+      ],
+      destinationBucket: bucket as any
+    });
+
     const lambdaa = new lambda.NodejsFunction(this, 'MySimpleAppLambda', {
       runtime: Runtime.NODEJS_14_X,
       entry: path.join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
       handler: 'getPhotos',
+      environment: {
+        PHOTO_BUCKET_NAME: bucket.bucketName,
+      }
     });
   
     new cdk.CfnOutput(this, 'MySimpleAppBucketNameExport', {
