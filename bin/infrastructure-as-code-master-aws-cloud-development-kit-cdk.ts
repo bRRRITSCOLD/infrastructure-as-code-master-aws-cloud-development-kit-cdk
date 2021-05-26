1310aaf7c08470f3855fd05c9ddfbe25185020fd
Blaine Richardson <blainerrichardson@gmail.com>
2021-05-26 18:44:45 -0400

    feat: secion-16: finishing

--- bin/infrastructure-as-code-master-aws-cloud-development-kit-cdk.ts
+++ bin/infrastructure-as-code-master-aws-cloud-development-kit-cdk.ts
@@ -4,18 +4,12 @@ import * as cdk from '@aws-cdk/core';
 import { InfrastructureAsCodeMasterAwsCloudDevelopmentKitCdkStack } from '../lib/infrastructure-as-code-master-aws-cloud-development-kit-cdk-stack';
 
 const app = new cdk.App();
-new InfrastructureAsCodeMasterAwsCloudDevelopmentKitCdkStack(app, 'InfrastructureAsCodeMasterAwsCloudDevelopmentKitCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+new InfrastructureAsCodeMasterAwsCloudDevelopmentKitCdkStack(app, 'InfrastructureAsCodeMasterAwsCloudDevelopmentKitCdkStack-dev', {
+  env: { region: "us-east-1" },
+  envName: 'dev'
+});
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new InfrastructureAsCodeMasterAwsCloudDevelopmentKitCdkStack(app, 'InfrastructureAsCodeMasterAwsCloudDevelopmentKitCdkStack-prod', {
+  env: { region: "us-east-1" },
+  envName: 'prod'
 });
